fad4d15d24f984394b6b4c2b9d164bbc3bb51353
hossainnadia <anchowdhury3562@gmail.com>
2024-09-18 01:46:06 +0600

    continue

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./Components/Navbar/NavBar";
+const App = () => {
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,74 @@
+import search_icon from "./search_icon.jpg";
+import basket_icon from "./basket_icon.jpg";
+import header_icon from "./header_icon.jpg";
+
+import { menu1 } from "./menu-1.jpg";
+import { menu2 } from "./menu-2.jpg";
+import { menu3 } from "./menu-3.jpg";
+import { menu4 } from "./menu-4.jpg";
+import { menu5 } from "./menu-5.jpg";
+import { menu6 } from "./menu-13.jpg";
+
+import { birani1 } from "./birani-1.jpg";
+import { birani2 } from "./birani-2.jpg";
+import { birani3 } from "./birani-3.jpg";
+import { birani4 } from "./birani-4.jpg";
+import { birani5 } from "./birani-5.jpg";
+
+import { bargar1 } from "./bargar-1.jpg";
+import { bargar2 } from "./bargar-2.jpg";
+import { bargar3 } from "./bargar-3.jpg";
+import { bargar4 } from "./bargar-4.jpg";
+import { bargar5 } from "./bargar-5.jpg";
+
+import { noduls1 } from "./noduls-1.jpg";
+import { noduls2 } from "./noduls-2.jpg";
+import { noduls3 } from "./noduls-3.jpg";
+import { noduls4 } from "./noduls-4.jpg";
+
+import { dimkari1 } from "./dimkari-1.jpg";
+import { dimkari2 } from "./dimkari-2.jpg";
+import { dimkari3 } from "./dimkari-3.jpg";
+import { dimkari4 } from "./dimkari-4.jpg";
+
+import { pizza1 } from "./pizza-1.jpg";
+import { pizza2 } from "./pizza-2.jpg";
+import { pizza3 } from "./pizza-3.jpg";
+import { pizza4 } from "./pizza-4.jpg";
+
+import add_icon_green from "./add_icon_green.jpg";
+import add_icon_white from "./add_icon_white.jpg";
+import add_store from "./add_store.jpg";
+import app_store from "./app_store.jpg";
+import linkedin_icon from "./linkedin_icon.jpg";
+import facebook_icon from "./facebook_icon .jpg";
+import twitter_icon from "./twitter_icon.jpg";
+import cross_icon from "./cross_icon.jpg";
+import selector_icon from "./selector_icon.jpg";
+import rating_icon from "./rating_icon.jpg";
+import profile_icon from "./profile_icon.jpg";
+import bag_icon from "./bag_icon.jpg";
+import logout_icon from "./logout_icon.jpg";
+import parcel_icon from "./parcel_icon.jpg";
+
+export const assets = () => {
+  search_icon,
+    basket_icon,
+    header_img,
+    rating_starts,
+    add_icon_green,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    bag_icon,
+    logout_icon,
+    parcel_icon;
+};
+
+export default assets;
